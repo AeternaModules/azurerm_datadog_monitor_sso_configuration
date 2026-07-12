e82616bbc2aeeb6a692cfef57a5d2e387c2962f9
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:09 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "datadog_monitor_sso_configurations" {
-  description = "All datadog_monitor_sso_configuration resources"
-  value       = azurerm_datadog_monitor_sso_configuration.datadog_monitor_sso_configurations
-}
 output "datadog_monitor_sso_configurations_datadog_monitor_id" {
-  description = "List of datadog_monitor_id values across all datadog_monitor_sso_configurations"
-  value       = [for k, v in azurerm_datadog_monitor_sso_configuration.datadog_monitor_sso_configurations : v.datadog_monitor_id]
+  description = "Map of datadog_monitor_id values across all datadog_monitor_sso_configurations, keyed the same as var.datadog_monitor_sso_configurations"
+  value       = { for k, v in azurerm_datadog_monitor_sso_configuration.datadog_monitor_sso_configurations : k => v.datadog_monitor_id }
 }
 output "datadog_monitor_sso_configurations_enterprise_application_id" {
-  description = "List of enterprise_application_id values across all datadog_monitor_sso_configurations"
-  value       = [for k, v in azurerm_datadog_monitor_sso_configuration.datadog_monitor_sso_configurations : v.enterprise_application_id]
+  description = "Map of enterprise_application_id values across all datadog_monitor_sso_configurations, keyed the same as var.datadog_monitor_sso_configurations"
+  value       = { for k, v in azurerm_datadog_monitor_sso_configuration.datadog_monitor_sso_configurations : k => v.enterprise_application_id }
 }
 output "datadog_monitor_sso_configurations_login_url" {
-  description = "List of login_url values across all datadog_monitor_sso_configurations"
-  value       = [for k, v in azurerm_datadog_monitor_sso_configuration.datadog_monitor_sso_configurations : v.login_url]
+  description = "Map of login_url values across all datadog_monitor_sso_configurations, keyed the same as var.datadog_monitor_sso_configurations"
+  value       = { for k, v in azurerm_datadog_monitor_sso_configuration.datadog_monitor_sso_configurations : k => v.login_url }
 }
 output "datadog_monitor_sso_configurations_name" {
-  description = "List of name values across all datadog_monitor_sso_configurations"
-  value       = [for k, v in azurerm_datadog_monitor_sso_configuration.datadog_monitor_sso_configurations : v.name]
+  description = "Map of name values across all datadog_monitor_sso_configurations, keyed the same as var.datadog_monitor_sso_configurations"
+  value       = { for k, v in azurerm_datadog_monitor_sso_configuration.datadog_monitor_sso_configurations : k => v.name }
 }
 output "datadog_monitor_sso_configurations_single_sign_on" {
-  description = "List of single_sign_on values across all datadog_monitor_sso_configurations"
-  value       = [for k, v in azurerm_datadog_monitor_sso_configuration.datadog_monitor_sso_configurations : v.single_sign_on]
+  description = "Map of single_sign_on values across all datadog_monitor_sso_configurations, keyed the same as var.datadog_monitor_sso_configurations"
+  value       = { for k, v in azurerm_datadog_monitor_sso_configuration.datadog_monitor_sso_configurations : k => v.single_sign_on }
 }
 output "datadog_monitor_sso_configurations_single_sign_on_enabled" {
-  description = "List of single_sign_on_enabled values across all datadog_monitor_sso_configurations"
-  value       = [for k, v in azurerm_datadog_monitor_sso_configuration.datadog_monitor_sso_configurations : v.single_sign_on_enabled]
+  description = "Map of single_sign_on_enabled values across all datadog_monitor_sso_configurations, keyed the same as var.datadog_monitor_sso_configurations"
+  value       = { for k, v in azurerm_datadog_monitor_sso_configuration.datadog_monitor_sso_configurations : k => v.single_sign_on_enabled }
 }
 
